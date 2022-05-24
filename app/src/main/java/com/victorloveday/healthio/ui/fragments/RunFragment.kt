4db86edd94bc683c9b07e1d5689bda7e98e31c6f
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-05-24 17:22:18 +0100

    Feat: add location permission

--- app/src/main/java/com/victorloveday/healthio/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/victorloveday/healthio/ui/fragments/RunFragment.kt
@@ -1,13 +1,104 @@
 package com.victorloveday.healthio.ui.fragments
 
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.victorloveday.healthio.R
+import com.victorloveday.healthio.databinding.FragmentRunBinding
 import com.victorloveday.healthio.ui.viewmodels.MainViewModel
+import com.victorloveday.healthio.utils.constants.Constant.REQUEST_CODE_LOCATION_PERMISSION
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment: Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: FragmentRunBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRunBinding.bind(view)
+
+        //request permissions
+        requestPermissions()
+
+        binding.setUp.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_setupFragment)
+        }
+
+        binding.newRun.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+
+    }
+
+    private fun requestPermissions() {
+        if (hasLocationPermissions(requireContext())) {
+            return
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "This app needs permission to your location to function properly",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }else {
+            EasyPermissions.requestPermissions(
+                this,
+                "This app needs permission to your location to function properly",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+
+        }
+    }
+
+    private fun hasLocationPermissions(context: Context) =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+//        TODO("Not yet implemented")
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
 }
--- app/src/main/java/com/victorloveday/healthio/utils/constants/Constant.kt
+++ app/src/main/java/com/victorloveday/healthio/utils/constants/Constant.kt
@@ -2,4 +2,7 @@ package com.victorloveday.healthio.utils.constants
 
 object Constant {
     const val DATABASE_NAME = "healthio_db"
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
+
+
 }
