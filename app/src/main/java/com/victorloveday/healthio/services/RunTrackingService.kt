3ed7060616894049ac02c758ecad7d259219662a
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-05-25 13:00:34 +0100

    Feat: tracking and updating user's location

--- app/src/main/java/com/victorloveday/healthio/services/RunTrackingService.kt
+++ app/src/main/java/com/victorloveday/healthio/services/RunTrackingService.kt
@@ -1,5 +1,7 @@
 package com.victorloveday.healthio.services
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
@@ -7,13 +9,25 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import androidx.core.content.getSystemService
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import com.victorloveday.healthio.R
 import com.victorloveday.healthio.ui.MainActivity
+import com.victorloveday.healthio.utils.RunTrackingUtility
+import com.victorloveday.healthio.utils.constants.Constant.FASTEST_LOCATION_UPDATE_INTERVAL
+import com.victorloveday.healthio.utils.constants.Constant.LOCATION_UPDATE_INTERVAL
 import com.victorloveday.healthio.utils.constants.Constant.NOTIFICATION_ID
 import com.victorloveday.healthio.utils.constants.Constant.PAUSE_RUN_SERVICE
 import com.victorloveday.healthio.utils.constants.Constant.RESUME_OR_START_RUN_SERVICE
@@ -21,20 +35,45 @@ import com.victorloveday.healthio.utils.constants.Constant.SHOW_TRACKING_FRAGMEN
 import com.victorloveday.healthio.utils.constants.Constant.STOP_RUN_SERVICE
 import com.victorloveday.healthio.utils.constants.Constant.TRACKING_NOTIFICATION_CHANNEL_ID
 import com.victorloveday.healthio.utils.constants.Constant.TRACKING_NOTIFICATION_CHANNEL_NAME
+import pub.devrel.easypermissions.EasyPermissions
 import timber.log.Timber
 
-class RunTrackingService: LifecycleService() {
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
+class RunTrackingService : LifecycleService() {
 
     var isFirstRun = true
 
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
-            when(it.action) {
+            when (it.action) {
                 RESUME_OR_START_RUN_SERVICE -> {
                     if (isFirstRun) {
-                        startForegroundService()
+                         startForegroundService()
                         isFirstRun = false
-                    }else Timber.d("Resume service")
+                    } else Timber.d("Resume service")
 
                 }
                 PAUSE_RUN_SERVICE -> {
@@ -48,8 +87,66 @@ class RunTrackingService: LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking == true) {
+            if (hasLocationPermissions(this)) {
+                val request = LocationRequest().apply {
+                     interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_UPDATE_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        }else {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult?) {
+            super.onLocationResult(result)
+
+            if (isTracking.value!!) {
+                result?.locations?.let { locations ->
+                    for (location in locations) {
+                        pathPoints(location)
+                        Timber.d("New Location: ${location.latitude}, ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun pathPoints(location: Location?) {
+        location?.let {
+            val position = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(position)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun emptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
+
     private fun startForegroundService() {
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        //add empty line from start
+        emptyPolyline()
+
+        isTracking.postValue(true)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel(notificationManager)
@@ -66,14 +163,20 @@ class RunTrackingService: LifecycleService() {
         startForeground(NOTIFICATION_ID, notificationBuilder.build())
     }
 
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(notificationManager: NotificationManager) {
-        val channel = NotificationChannel(TRACKING_NOTIFICATION_CHANNEL_ID, TRACKING_NOTIFICATION_CHANNEL_NAME, IMPORTANCE_LOW)
+        val channel = NotificationChannel(
+            TRACKING_NOTIFICATION_CHANNEL_ID,
+            TRACKING_NOTIFICATION_CHANNEL_NAME,
+            IMPORTANCE_LOW
+        )
 
         notificationManager.createNotificationChannel(channel)
     }
 
-    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(this,
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
         0,
         Intent(this, MainActivity::class.java).also {
             it.action = SHOW_TRACKING_FRAGMENT
@@ -85,4 +188,22 @@ class RunTrackingService: LifecycleService() {
 //            it.action = SHOW_TRACKING_FRAGMENT
 //        }, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
 //    )
+
+
+    private fun hasLocationPermissions(context: Context) =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+
 }
