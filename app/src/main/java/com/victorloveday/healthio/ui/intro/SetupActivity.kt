e6703ba4c84eea307f34172376a898c34894a2cb
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-05-31 20:58:38 +0100

    Fix: back arrow button on setup activity

--- app/src/main/java/com/victorloveday/healthio/ui/intro/SetupActivity.kt
+++ app/src/main/java/com/victorloveday/healthio/ui/intro/SetupActivity.kt
@@ -1,21 +1,14 @@
 package com.victorloveday.healthio.ui.intro
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.victorloveday.healthio.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.victorloveday.healthio.database.UserManager
 import com.victorloveday.healthio.databinding.ActivitySetupBinding
-import com.victorloveday.healthio.ui.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import android.view.View
+import android.widget.Toast
+
 
 @AndroidEntryPoint
 class SetupActivity : AppCompatActivity() {
@@ -31,11 +24,15 @@ class SetupActivity : AppCompatActivity() {
         //initialize user manager
         userManager = UserManager(this)
 
-        binding.goBack.setOnClickListener {
+        val toolbar = binding.toolbar as Toolbar
+        setSupportActionBar(toolbar);
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener {
             finish()
         }
-
-
     }
 
 
